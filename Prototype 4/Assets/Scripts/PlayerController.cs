e35c1402057c1008dee6b14c8addb2d0013b89b4
Yasin Mahdizadeh <yasin.mahdizade@gmail.com>
2020-05-11 20:43:42 +0430

    Session 1 Done!

---
+++ Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private float forwardInput;
+
+    public float speed = 5;
+    private Rigidbody playerRb;
+
+    private GameObject focalPoint;
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+        forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * Time.deltaTime * speed);
+    }
+}
---
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float horizontalInput;
+    public float rotationSpeed = 15;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(Vector3.up, horizontalInput * Time.deltaTime * rotationSpeed);
+    }
+}
